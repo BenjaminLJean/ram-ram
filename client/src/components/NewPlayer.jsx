6aed704a706932d1e9143059a47c8ff95e88e80f
Ben Jean <leroan@Bens-MacBook-Pro.local>
2021-09-27 14:20:38 -0400

    added a bit more to the button and input, also fixed background clipping issue

--- client/src/components/NewPlayer.jsx
+++ client/src/components/NewPlayer.jsx
@@ -43,7 +43,7 @@ export default function NewPlayer() {
 
   return (
     <div>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Avatar Name" />
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Avatar Name" className="input" />
       <button className="playbutton" onClick={handleSubmit}>Play!</button>
     </div>
   )
